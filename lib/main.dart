13afa49cacb0ff505b700dfcf2311b4be92c5e9c
Shahzalal <Shahzalalkhan@gmail.com>
2025-08-07 18:31:15 +0600

    Add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
+import 'dart:ui';
+
 import 'package:crafty_bay_ecommerce/app/app.dart';
+import 'package:crafty_bay_ecommerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const CraftyBayEcommerce());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  //Framework error
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  // Framework async error
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(CraftyBayEcommerce());
 }
-
-
