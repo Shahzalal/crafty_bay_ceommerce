7dca287b9267842f8068572262a2d22ca09d8a71
Shahzalal <Shahzalalkhan@gmail.com>
2025-08-08 12:26:34 +0600

    Home screen ui design done

--- lib/features/auth/ui/screens/home_screen.dart
+++ lib/features/auth/ui/screens/home_screen.dart
@@ -4,13 +4,13 @@ import 'package:crafty_bay_ecommerce/features/auth/ui/widgets/home_carousel_slid
 import 'package:crafty_bay_ecommerce/features/auth/ui/widgets/product_category_item.dart';
 import 'package:crafty_bay_ecommerce/features/auth/ui/widgets/product_search_bar.dart';
 import 'package:crafty_bay_ecommerce/features/common/ui/controller/main_bottom_nav_controller.dart';
+import 'package:crafty_bay_ecommerce/features/common/ui/widgets/product_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -33,13 +33,19 @@ class _HomeScreenState extends State<HomeScreen> {
                 HomeCarouselSlider(),
                 const SizedBox(height: 16),
 
-                _buildSectionHeader(title: 'Categories', onTapApp: () {
-                  Get.find<MainBottomNavController>().moveToCategory();
-                }),
+                _buildSectionHeader(
+                  title: 'Categories',
+                  onTapApp: () {
+                    Get.find<MainBottomNavController>().moveToCategory();
+                  },
+                ),
                 _getCategoryList(),
                 _buildSectionHeader(title: 'Popular', onTapApp: () {}),
+                _getPopularProducts(),
                 _buildSectionHeader(title: 'Special', onTapApp: () {}),
+                _getSpecialProducts(),
                 _buildSectionHeader(title: 'New', onTapApp: () {}),
+                _getNewProducts(),
               ],
             ),
           ),
@@ -86,6 +92,40 @@ class _HomeScreenState extends State<HomeScreen> {
           return ProductCategoryItem();
         },
         separatorBuilder: (context, index) => const SizedBox(width: 8),
+      ),
+    );
+  }
+
+  Widget _getPopularProducts() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        spacing: 8,
+        children: [1, 2, 3, 4].map((e) => ProductCard()).toList(),
+      ),
+    );
+  }
+
+  Widget _getSpecialProducts() {
+    return SizedBox(
+      height: 185,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return ProductCard();
+        },
+        separatorBuilder: (context, index) => const SizedBox(width: 8),
+      ),
+    );
+  }
+
+  Widget _getNewProducts() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        spacing: 8,
+        children: [1, 2, 3, 4, 5].map((e) => ProductCard()).toList(),
       ),
     );
   }
