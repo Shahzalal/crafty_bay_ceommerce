eefee476dbe5aa60f4764ebbcfc3574a57d447d7
Shahzalal <Shahzalalkhan@gmail.com>
2025-08-20 17:44:22 +0600

    Add auth controller

--- lib/app/controller_binder.dart
+++ lib/app/controller_binder.dart
@@ -1,13 +1,17 @@
 import 'package:crafty_bay_ecommerce/core/services/network/network_client.dart';
 import 'package:crafty_bay_ecommerce/features/auth/ui/controller/sign_up_controller.dart';
+import 'package:crafty_bay_ecommerce/features/auth/ui/screens/login_screen.dart';
+import 'package:crafty_bay_ecommerce/features/common/controller/auth_controller.dart';
 import 'package:crafty_bay_ecommerce/features/common/ui/controller/main_bottom_nav_controller.dart';
 import 'package:get/get.dart';
 
 import '../features/auth/ui/controller/verity_otp_controller.dart';
 
 class ControllerBinder extends Bindings {
+  final AuthController _authController = AuthController();
   @override
   void dependencies() {
+    Get.put(_authController);
     Get.put(MainBottomNavController());
     Get.put(
       NetworkClient(
@@ -19,9 +23,14 @@ class ControllerBinder extends Bindings {
     Get.put(VerityOtpController());
   }
 
-  void _onUnAuthorize() {}
-  final Map<String, String> _commonHeaders = {
+  Future<void> _onUnAuthorize() async {
+    await _authController.clearUserData();
+    Get.to(()=>LoginScreen());
+  }
+  Map<String, String>get  _commonHeaders {
+    return{
     'content-type': 'application/json',
-    'token': '',
-  };
+      'token':_authController.accessToken ?? '',
+    };
+  }
 }
